25fb3b4bea26f7ff70b7dcfb596a47f9d6ded8fa
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-04-04 15:35:15 +0800

    Add lengthOfLastWord method to Solution class for computing the length of the last word in a string.

---
+++ lengthOfLastWord.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words_array = []
+        word = ""
+        for char in s:
+            if char != " ":
+                word += char
+            elif word:
+                words_array.append(word)
+                word = ""
+        # After the loop, if there's still a word left add it to the array 
+        if word:
+            words_array.append(word)
+        if words_array:
+            return len(words_array[-1])
+        else:
+            return 0
+
+test_case = "Hello World  "
+solution = Solution()
+print(solution.lengthOfLastWord(test_case))  # Output should be 5
